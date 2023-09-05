c24b3d8234b5e15423cc69061bc8e6c152ba2af7
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-09-05 12:28:34 +0200

    better log messages for dump (prefixes)

--- nimble/screamingchannels/src/dump.c
+++ nimble/screamingchannels/src/dump.c
@@ -50,27 +50,28 @@ void dump_ble_encryption_block(struct ble_encryption_block *ecb)
     console_printf("\n");
 #endif
 }
+
 void dump_ble_ll_conn_sm(struct ble_ll_conn_sm *connsm)
 {
 #if MYNEWT_VAL(SC_LOG_DUMP_ENABLE)
 #if MYNEWT_VAL(SC_LOG_TRACE_ENABLE)
-    console_printf("dump_ble_ll_conn_sm(connsm=%p)\n", connsm);
+    console_printf("[dump.c] dump_ble_ll_conn_sm(connsm=%p)\n", connsm);
 #endif
     // Reference in ble_ll_conn.h
-    console_printf("conn_handle=%"     PRIu16 "\n", connsm->conn_handle);
-    console_printf("conn_state=%"      PRIu8  "\n", connsm->conn_state);
-    console_printf("conn_role=%"       PRIu8  "\n", connsm->conn_role);
-    console_printf("channel_id=%"      PRIu16 "\n", connsm->channel_id);
-    console_printf("data_chan_index=%" PRIu8  "\n", connsm->data_chan_index);
-    console_printf("anchor_point=%"    PRIu32 "\n", connsm->anchor_point);
-    console_printf("peer_addr=");
+    console_printf("[v] conn_handle=%"     PRIu16 "\n", connsm->conn_handle);
+    console_printf("[v] conn_state=%"      PRIu8  "\n", connsm->conn_state);
+    console_printf("[v] conn_role=%"       PRIu8  "\n", connsm->conn_role);
+    console_printf("[v] channel_id=%"      PRIu16 "\n", connsm->channel_id);
+    console_printf("[v] data_chan_index=%" PRIu8  "\n", connsm->data_chan_index);
+    console_printf("[v] anchor_point=%"    PRIu32 "\n", connsm->anchor_point);
+    console_printf("[v] peer_addr=");
     dump_ble_addr(connsm->peer_addr);
     console_printf("\n");
-    console_printf("chanmap=");
+    console_printf("[v] chanmap=");
     dump_ble_chanmap(connsm->chanmap);
     console_printf("\n");
     char * enc_state = get_ble_ll_conn_enc_state(connsm->enc_data.enc_state);
-    console_printf("enc_data.enc_state=%s\n", enc_state);
+    console_printf("[v] enc_data.enc_state=%s\n", enc_state);
 #endif
 }
 
@@ -78,22 +79,25 @@ void dump_ble_ll_conn_enc_data(struct ble_ll_conn_enc_data *enc_data)
 {
 #if MYNEWT_VAL(SC_LOG_DUMP_ENABLE)
 #if MYNEWT_VAL(SC_LOG_TRACE_ENABLE)
-    console_printf("dump_ble_ll_conn_enc_data(enc_data=%p)\n", enc_data);
+    console_printf("[dump.c] dump_ble_ll_conn_enc_data(enc_data=%p)\n", enc_data);
 #endif
     int cnt;
-    console_printf("\nLTK (key):");
+    console_printf("[v] LTK:enc_data->enc_block.key=0x");
     for (cnt = 0; cnt < 16; ++cnt) {
         console_printf("%02x", enc_data->enc_block.key[cnt]);
     }
-    console_printf("\nSKD (plain_text):");
+    console_printf("\n");
+    console_printf("[v] SKD:enc_data->enc_block.plain_text=0x");
     for (cnt = 0; cnt < 16; ++cnt) {
         console_printf("%02x", enc_data->enc_block.plain_text[cnt]);
     }
-    console_printf("\nSession Key (cipher_text):");
+    console_printf("\n");
+    console_printf("[v] SK:enc_data->enc_block.cipher_text=0x");
     for (cnt = 0; cnt < 16; ++cnt) {
         console_printf("%02x", enc_data->enc_block.cipher_text[cnt]);
     }
-    console_printf("\nIV:");
+    console_printf("\n");
+    console_printf("[v] IV:enc_data->iv=0x");
     for (cnt = 0; cnt < 8; ++ cnt) {
         console_printf("%02x", enc_data->iv[cnt]);
     }
@@ -112,7 +116,7 @@ void dump_ble_addr(uint8_t * addr)
 void dump_ble_chanmap(uint8_t * chanmap)
 {
 #if MYNEWT_VAL(SC_LOG_DUMP_ENABLE)
-    // Reference in ble_ll.h
+    // Reference in ble_ll_conn.h::243 and ble_ll.h::94.
     console_printf("0x%02hhx%02hhx%02hhx%02hhx%02hhx", chanmap[4], chanmap[3], chanmap[2], chanmap[1], chanmap[0]);
 #endif
 }
