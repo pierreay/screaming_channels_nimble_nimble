ea562ef0bd3b890132040fdf002a15d7b7cae0eb
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-09-05 14:37:05 +0200

    allows to set if current conn is sc train or sc attack in misc

--- nimble/screamingchannels/include/screamingchannels/misc.h
+++ nimble/screamingchannels/include/screamingchannels/misc.h
@@ -7,6 +7,13 @@
 extern "C" {
 #endif
 
+// Global variables used accross other Nimble packages used to know if the
+// current connection is a Screaming Channels attack or profile collection.
+extern int IS_SC_TRAIN;
+extern int IS_SC_ATTACK;
+
+void set_is_sc_train(uint8_t * chanmap);
+void set_is_sc_attack(uint8_t * chanmap);
 int is_sc_chanmap(uint8_t * chanmap);
 
 #ifdef __cplusplus
--- nimble/screamingchannels/src/misc.c
+++ nimble/screamingchannels/src/misc.c
@@ -4,6 +4,19 @@
 
 #define SC_CHANMAP 0x300
 
+int IS_SC_TRAIN  = 0;
+int IS_SC_ATTACK = 0;
+
+void set_is_sc_train(uint8_t * chanmap)
+{
+    IS_SC_TRAIN = is_sc_chanmap(chanmap);
+}
+
+void set_is_sc_attack(uint8_t * chanmap)
+{
+    IS_SC_ATTACK = is_sc_chanmap(chanmap);
+}
+
 // Usage: is_sc_chanmap(&connsm->chanmap))
 // Return 1 if it is Screaming Channels channel map, 0 otherwise.
 int is_sc_chanmap(uint8_t * chanmap)
