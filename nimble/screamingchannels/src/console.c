9a83bbbbec3875a883a346f10843730242ea650c
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-10-05 18:45:26 +0200

    [console] start writing custom key material into nimble database

--- nimble/screamingchannels/src/console.c
+++ nimble/screamingchannels/src/console.c
@@ -6,6 +6,8 @@
 #include "screamingchannels/input.h"
 #include "screamingchannels/dump.h"
 #include "console/console.h"
+#include "nimble/ble.h"
+#include "host/ble_store.h"
 
 #define INPUT_BASE_OFFSET 2 // Length of k: and p: used in commands.
 
@@ -57,9 +59,34 @@ screamingchannels_process_input(struct os_event *ev)
         dump_sc_state();
     }
     else if (!strcmp(line, "input_sub")) {
-        SC_INPUT_MODE = SC_INPUT_MODE_SUB;
-        // TODO: Register KS and PT into Nimble security DB.
-        SC_INPUT_SUB_OK = 1;
+        // Register key into the Nimble security database.
+        // TODO: Continue to properly set value_sec and peer_addr before to move on AES.
+        struct ble_store_value_sec value_sec;
+        ble_addr_t peer_addr;
+        peer_addr.type = 0;
+        peer_addr.val[5] = 0x00;
+        peer_addr.val[4] = 0x19;
+        peer_addr.val[3] = 0x0e;
+        peer_addr.val[2] = 0x19;
+        peer_addr.val[1] = 0x79;
+        peer_addr.val[0] = 0xd8;
+        value_sec.peer_addr = peer_addr;
+        value_sec.key_size = 16;
+        value_sec.ediv = 0xdead;
+        value_sec.rand_num = 0xdeadbeefdeadbeef;
+        value_sec.ltk[0] = 0xde;
+        value_sec.ltk[1] = 0xad;
+        value_sec.ltk[2] = 0xbe;
+        value_sec.ltk[3] = 0xef;
+        value_sec.ltk_present = 1;
+        int rc = ble_store_write_our_sec(&value_sec);
+        if (rc == 0) {
+            SC_INPUT_MODE = SC_INPUT_MODE_SUB;
+            SC_INPUT_SUB_OK = 1;
+        }
+        else {
+            console_printf("error: rc=%d", rc);
+        }
     }
     else if (line[0] == 'k' && line[1] == ':') {
         str_hex_to_uint8_dec(line + INPUT_BASE_OFFSET, SC_INPUT_KS, INPUT_SIZE);
